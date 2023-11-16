7d19e89da3ed45e5c7cfef179cd7e4df3e921626
Edwin2402.github.io <20211902@itla.edu.do>
2023-11-16 14:34:59 -0400

    Primer commit de Edwin Díaz

---
+++ __tests__/index.test.js
@@ -0,0 +1,10 @@
+
+const request = require('supertest');
+const app = require('../index');
+
+describe('Pruebas Unitarias del Hola Mundo', () => {
+  it('Debería retornar ¡Hola, mundo! en la ruta "/"', async () => {
+    const response = await request(app).get('/');
+    expect(response.text).toBe('¡Hola, mundo!');
+  });
+});
