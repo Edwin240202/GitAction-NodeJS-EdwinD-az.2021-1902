3942d0d9157fde45dd14851ca6834023c9214e36
Edwin2402.github.io <20211902@itla.edu.do>
2023-11-21 20:20:08 -0400

    prueba2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function saludarConNombreYMatricula() {
     const nombre = 'EDWIN DAVID DÍAZ MENDOZA';
-    const matricula = '2021-190';
+    const matricula = '2021-1902';
     return `Hola, mi nombre es ${nombre} y mi matrícula es ${matricula}.`;
 }
 
